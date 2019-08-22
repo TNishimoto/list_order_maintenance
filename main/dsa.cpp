624318ddc057119b5da4e6dc83195441b3be4f99
Nishimoto <a32b16c4@gmail.com>
2019-08-22 19:16:13 +0900

    dsa

--- main/dsa.cpp
+++ main/dsa.cpp
@@ -13,16 +13,23 @@ int main(int argc, char *argv[]){
     
     cmdline::parser p;
     p.add<string>("input_file", 'i', "input file name", true);
+    p.add<string>("output_file", 'o', "output file name", false, "");
+    
     p.parse_check(argc, argv);
     std::string inputFile = p.get<std::string>("input_file");
+    std::string outputFile = p.get<std::string>("output_file");
+    if(outputFile.size() == 0){
+        outputFile = inputFile + ".sa64";
+    }
 
     auto start = std::chrono::system_clock::now();
     std::vector<uint8_t> text;
     stool::load_vector(inputFile, text, false);
-    auto dsa = stool::LO::construct_differential_suffix_array(text);
-    auto rlbwt = stool::LO::construct_rlbwt(dsa);
-    //stool::Printer::print(dsa);
-    std::cout << rlbwt.size() << std::endl;
+    std::vector<int64_t> dsa = stool::LO::construct_differential_suffix_array(text);
+    
+    stool::write_vector(outputFile, dsa, false);
+
+    
     auto end = std::chrono::system_clock::now();
     double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
@@ -30,9 +37,10 @@ int main(int argc, char *argv[]){
     std::cout << "\033[36m";
     std::cout << "=============RESULT===============" << std::endl;
     std::cout << "File : " << inputFile << std::endl;
+    std::cout << "Output file : " << outputFile << std::endl;
     std::cout << "The length of the input text : " << text.size() << std::endl;
     double charperms = (double)text.size() / elapsed;
-    std::cout << "The number of RLBWT : " << rlbwt.size() << std::endl;
+    //std::cout << "The number of RLBWT : " << rlbwt.size() << std::endl;
     std::cout << "Excecution time : " << ((uint64_t)elapsed) << "ms";
     std::cout << "[" << charperms << "chars/ms]" << std::endl;
     std::cout << "==================================" << std::endl;
